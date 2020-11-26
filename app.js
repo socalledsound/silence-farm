fcc3b96029ed086c377e70ec8e2503c1c1aaedc6
Chris Kubick <socalledsound@gmail.com>
2020-11-26 11:36:51 -0800

    firebase storage and firestore integration

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-const drop = document.querySelector('#drop');
-const gallery = document.querySelector('#gallery');
+const firebaseConfig = {
+    apiKey: "AIzaSyDaML5MLtlBbV3CtiJb-6mHDdRGlNiU-0U",
+    authDomain: "new-image-gallery.firebaseapp.com",
+    databaseURL: "https://new-image-gallery.firebaseio.com",
+    projectId: "new-image-gallery",
+    storageBucket: "new-image-gallery.appspot.com",
+    messagingSenderId: "910910072207",
+    appId: "1:910910072207:web:2c3a9570b4656d5bc117fa"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const imagesRef = db.collection('images');
+
+  const storage = firebase.storage();
+
+  imagesRef.onSnapshot( snapshot => {
+      let update = snapshot.docChanges();
+      console.log(update);
+      update.forEach( image => {
+        console.log(image);
+        console.log(image.doc);
+        console.log(image.doc.data()); 
+        console.log(image.doc.data().imageURL);  
+        const imgPath = image.doc.data().imageURL;
+        addToGallery(imgPath);
+      })
+  })
+
+
+
+    const drop = document.querySelector('#drop');
+    const gallery = document.querySelector('#gallery');
 
 window.addEventListener('dragover', (e) => {
     e.preventDefault();
@@ -17,7 +48,11 @@ drop.addEventListener('drop', (e) => {
 
     reader.addEventListener('load', (e) => {
         let fileURL = reader.result;
-        addToGallery(fileURL);
+        const metadata = { contentType : file.type};
+
+        uploadFile(file, metadata);
+        
+        // addToGallery(fileURL);
     })
 
     if(file){
@@ -37,6 +72,14 @@ function addToGallery(url){
     imgElement.src = url;
     newGalleryItem.appendChild(imgElement);
     gallery.appendChild(newGalleryItem);
+}
 
-
+function uploadFile(file, metadata){
+    const uniqueID = window.crypto.getRandomValues(new Uint32Array(1))[0]
+    const filePath = `images/${uniqueID}.jpg`;
+    const upload = storage.ref().child(filePath).put(file, metadata).then((snapshot) => {
+        snapshot.ref.getDownloadURL().then(downloadURL => {
+            imagesRef.add({imageURL: downloadURL})
+        })
+    })  
 }
