cf5e3bf9d46130d747a2452d1d6768c1f6cf546c
Chris Kubick <socalledsound@gmail.com>
2020-12-01 17:45:28 -0800

    remove config

---
+++ public/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4EsewDo35Id3aBaOGANh_wJZ_s9J2thk",
+    authDomain: "silence-farm.firebaseapp.com",
+    databaseURL: "https://silence-farm.firebaseio.com",
+    projectId: "silence-farm",
+    storageBucket: "silence-farm.appspot.com",
+    messagingSenderId: "738373102064",
+    appId: "1:738373102064:web:46428009f7d90a7f971535",
+    measurementId: "G-ZLZW2R060L"
+  };
